05c7ed6e74a00aee43e230dd4f49dc7679d2f12d
Jakub Vonkomer <kubo@vonsch.sk>
2019-11-18 15:37:56 +0100

    add pyinstaller script for building the application
    ignore temporary and result files

---
+++ build_app.py
@@ -0,0 +1,21 @@
+# short app for building standalone application using pyinstaller
+
+import subprocess
+import os
+import version as vs
+
+targetFilename = vs.appTitle.replace('.','_')+'.exe'
+
+pathToPyinstaller = "C:\Program Files (x86)\Microsoft Visual Studio\Shared\Python36_64\Scripts\pyinstaller.exe"
+args = [pathToPyinstaller,'--onefile','main.py']
+
+print('Building the app...')
+proc = subprocess.run(args,shell=True, check=True)
+print(proc)
+
+
+print('Renaming to ',targetFilename)
+os.rename('dist/main.exe','dist/'+targetFilename)
+
+
+
